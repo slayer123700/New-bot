70f4283134f238a914e229b673d903e180cab283
- ♔ 𝐒 𝐇 𝐈 𝐕 𝐀 𝐍 𝐆 🜲 ˹ 𝐎ᴘ ˼﹛🇨🇦﹜≈ 💸 <Shivmis165@gmail.com>
2025-06-17 18:03:07 +0530

    Add files via upload

--- AnieXEricaMusic/__init__.py
+++ AnieXEricaMusic/__init__.py
@@ -1,26 +1,7 @@
-from AnieXEricaMusic.core.bot import AMBOT
-from AnieXEricaMusic.core.dir import dirr
-from AnieXEricaMusic.core.git import git
-from AnieXEricaMusic.core.userbot import Userbot
-from AnieXEricaMusic.misc import dbb, heroku
-
-from .logging import LOGGER
-
-dirr()
-git()
-dbb()
-heroku()
-
-app = AMBOT()
-userbot = Userbot()
-
-
-from .platforms import *
-
-Apple = AppleAPI()
-Carbon = CarbonAPI()
-SoundCloud = SoundAPI()
-Spotify = SpotifyAPI()
-Resso = RessoAPI()
-Telegram = TeleAPI()
-YouTube = YouTubeAPI()
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
