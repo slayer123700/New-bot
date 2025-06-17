57882a2561f5eb9cdbd8a7a5524e9bc29e5a6c79
slayer <meerutuniversity996@gmail.com>
2025-06-17 18:29:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -62,18 +62,18 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/4e68b6d15e042249fb284.png")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://telegra.ph/file/4e68b6d15e042249fb284.png")
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = getenv("STATS_IMG_URL", "https://telegra.ph/file/4e68b6d15e042249fb284.png")
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+START_IMG_URL =  "https://i.ibb.co/gFg5XstQ/photo-2025-05-24-04-00-24-7507857220025974820.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/bggrlh.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/iffmnv.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/f3yuiy.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/urv7wi.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/6khxhw.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/2tcim5.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/bggrlh.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/iffmnv.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/6khxhw.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/jkqyg2.jpg"
+
 
 def time_to_seconds(time):
     stringt = str(time)
