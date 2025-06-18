c902be4995643cfeb5539ea29399bfc34e652d4f
slayer <meerutuniversity996@gmail.com>
2025-06-18 11:32:02 +0530

    Update utag.py

--- AnieXEricaMusic/plugins/shivang/utag.py
+++ AnieXEricaMusic/plugins/shivang/utag.py
@@ -6,6 +6,7 @@ from pyrogram.errors import UserNotParticipant, FloodWait
 from pyrogram.types import Message
 
 from AnieXEricaMusic import app
+from ANNIEMUSIC.utils.admin_filters import admin_filter
 
 spam_chats = set()
 
